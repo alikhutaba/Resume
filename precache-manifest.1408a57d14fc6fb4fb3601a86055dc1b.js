291fe9839926603e7c4ded481d0424a3ab297e31
ali khutaba <khutaba.ail@outlook.com>
2020-12-09 14:22:09 +0200

    Updates

---
+++ precache-manifest.1408a57d14fc6fb4fb3601a86055dc1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e774871a702d34f23133701a4fd7b99c",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "b925110351e260a8f78c",
+    "url": "/Resume/static/css/2.956e9ece.chunk.css"
+  },
+  {
+    "revision": "09071c4659dffe4d05ca",
+    "url": "/Resume/static/css/main.59cbdfe5.chunk.css"
+  },
+  {
+    "revision": "b925110351e260a8f78c",
+    "url": "/Resume/static/js/2.7cee4e59.chunk.js"
+  },
+  {
+    "revision": "3a439d0148361576cf719017f69da164",
+    "url": "/Resume/static/js/2.7cee4e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09071c4659dffe4d05ca",
+    "url": "/Resume/static/js/main.2c4e448c.chunk.js"
+  },
+  {
+    "revision": "c0a18ddd6173a6d0c65f",
+    "url": "/Resume/static/js/runtime-main.c5f819f1.js"
+  },
+  {
+    "revision": "85cdacc9e11d6288ff2e1f99c920d4d7",
+    "url": "/Resume/static/media/ERM.85cdacc9.png"
+  },
+  {
+    "revision": "af1d5f097b686a850bceebb68e14658d",
+    "url": "/Resume/static/media/ali-proflie.af1d5f09.png"
+  },
+  {
+    "revision": "f4cb7ba70af83192321a3839aa20db8c",
+    "url": "/Resume/static/media/lights.f4cb7ba7.jpeg"
+  },
+  {
+    "revision": "dd209bf0d4d7bef7baa4569e49e2e17b",
+    "url": "/Resume/static/media/pathfinding.dd209bf0.png"
+  },
+  {
+    "revision": "4dac4ace946eb482a7cb65bbcf2bdc9c",
+    "url": "/Resume/static/media/sorting.4dac4ace.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Resume/precache-manifest.fca801b7ed1b817e43fa68fbbfdaf88a.js"
+  "/Resume/precache-manifest.1408a57d14fc6fb4fb3601a86055dc1b.js"
 );
 
 self.addEventListener('message', (event) => {
